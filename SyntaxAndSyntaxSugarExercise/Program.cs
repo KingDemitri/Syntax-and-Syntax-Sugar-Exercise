527a832d3db84c6acd2a3cef5715ff09918de902
Demitri Cameron <oblivion9794@gmail.com>
2023-01-15 11:58:13 -0500

    initial commit

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -5,6 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            var answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + "is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + "is greater than or equal to nine";
+
+            //}
+
+            string response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine"; 
+            Console.WriteLine (response);
+
         }
     }
 }
